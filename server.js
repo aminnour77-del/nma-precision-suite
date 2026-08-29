ccfddad74811255fcead1b8fd78a24866a07c282
Noureddine amine <noureddine@MBPdiNoureddine.homenet.telecomitalia.it>
2026-08-29 20:14:40 +0200

    Aggiornamento completo NMA Suite con WebRTC e Telemetria

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.get('*', (req, res) => {
-  const fs = require('fs');
   const p1 = path.join(__dirname, 'public', 'index.html');
   const p2 = path.join(__dirname, 'src/public', 'index.html');
   if (fs.existsSync(p1)) res.sendFile(p1);
@@ -39,25 +36,24 @@ io.on('connection', (socket) => {
   socket.on('call-user', (data) => {
     socket.broadcast.emit('call-made', {
       offer: data.offer,
-      isVideo: data.isVideo,
-      username: data.username,
-      socket: socket.id
+      socket: socket.id,
+      user: data.user,
+      isVideo: data.isVideo
     });
   });
 
   socket.on('make-answer', (data) => {
-    io.to(data.to).emit('answer-made', {
-      answer: data.answer,
-      socket: socket.id
+    socket.to(data.to).emit('answer-made', {
+      socket: socket.id,
+      answer: data.answer
     });
   });
 
   socket.on('ice-candidate', (data) => {
-    if (data.to) {
-      io.to(data.to).emit('ice-candidate', { candidate: data.candidate, socket: socket.id });
-    } else {
-      socket.broadcast.emit('ice-candidate', { candidate: data.candidate, socket: socket.id });
-    }
+    socket.broadcast.emit('ice-candidate', {
+      candidate: data.candidate,
+      socket: socket.id
+    });
   });
 
   socket.on('end-call', () => {
@@ -66,11 +62,10 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     activeUsers.delete(socket.id);
-    socket.broadcast.emit('call-ended');
   });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server N.M.A. SYSTEMS pronto sulla porta ${PORT}`);
+  console.log(`Server NMA attivo sulla porta ${PORT}`);
 });
